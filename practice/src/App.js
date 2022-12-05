d6040fed090ea9de41f21bb2336d849f7c64f388
haminse <haminse@naver.com>
2022-12-05 14:01:28 -0800

    what the

---
+++ practice/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>Hello There~</h1>
+    <p>Hello!</p>
+    <p>Hello!</p>
+    <p>Hello!</p>
+    <p>Hello!</p>
+    <button>Click!</button>
+    </>
+  );
+}
+
+export default App;
